c8468591c130d1e92728285e9925ceb4378b6993
Geovanny Risco <geovannyalexanderr@gmail.com>
2023-04-22 01:18:58 +0200

    improve resfinder script

--- collect_args-resfinder.sh
+++ collect_args-resfinder.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # Run ResFinder4 for a certain sample
-output_dir="resfinder_results"
+base_folder="data"
+filename="1351.832.fna" # MASTER_WP4_TEAGASC_1__TGS01-01__bin.2.fa
 specie="Enterococcus faecalis"
-fasta_filepath="MASTER_WP4_TEAGASC_1__TGS01-01__bin.2.fa"
+output_dir="$base_folder/resfinder_results_$filename"
+fasta_filepath="$base_folder/example_inputs/$filename"
 python -m resfinder -o $output_dir \
     -s "$specie" -l 0.6 -t 0.8 --acquired --point -ifa $fasta_filepath
 
