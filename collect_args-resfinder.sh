620fd869fdc513aac338fbb5f91616ef67ff4ad7
Geovanny Risco <geovannyalexanderr@gmail.com>
2023-04-22 01:32:41 +0200

    add output documentation to resfinder script

--- collect_args-resfinder.sh
+++ collect_args-resfinder.sh
@@ -9,4 +9,15 @@ fasta_filepath="$base_folder/example_inputs/$filename"
 python -m resfinder -o $output_dir \
     -s "$specie" -l 0.6 -t 0.8 --acquired --point -ifa $fasta_filepath
 
-
+# Results will look like as follows:
+# - <fasta_filename>.json: json file with detailed results, including non-resistance genes.
+# - pheno_table_species.txt: table with species specific AMR phenotypes.
+# - pheno_table.txt: table with all AMR phenotypes.
+# - ResFinder_Hit_in_genome_seq.fsa: fasta sequence of resistance gene hits found in the input data (query).
+# - ResFinder_Resistance_gene_seq.fsa: fasta sequence of resistance gene hits found in the database (reference).
+# - ResFinder_results_tab.txt: tab seperated table with predicted resistance genes.
+# - ResFinder_results.txt: predicted resistance genes grouped by antibiotic class and hit alignments to reference resistance genes.
+# - PointFinder_results.txt: tab seperated table with predicted point mutations leading to antibiotic resistance.
+# - PointFinder_table.txt: predicted point mutations grouped into genes to which they belong.
+#
+# More information about the results can be found here: https://cge.food.dtu.dk/services/ResFinder-4.1/output.php
