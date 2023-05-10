c013da571e776842bc429cde7fd37e794e660647
Geovanny Risco <geovannyalexanderr@gmail.com>
2023-05-11 01:30:00 +0200

    parse gene name instead of official geneID for variant calling

--- src/variant_calling/scripts/gff2bed.sh
+++ src/variant_calling/scripts/gff2bed.sh
@@ -4,5 +4,5 @@
 input_gff=$1
 output_bed=$2
 
-awk -F'\t' '$3 == "gene" {split($9, a, ";"); for (i in a) {if (a[i] ~ /^ID=/) {split(a[i], b, "="); print $1 "\t" $4 "\t" $5 "\t" b[2]}}}' \
+awk -F'\t' '$3 == "gene" {split($9, a, ";"); for (i in a) {if (a[i] ~ /^Name=/) {split(a[i], b, "="); print $1 "\t" $4 "\t" $5 "\t" b[2]}}}' \
 $input_gff > $output_bed
