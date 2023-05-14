2d58889c1817371f7c30cd8df80f8ac5a56d7297
Geovanny Risco <geovannyalexanderr@gmail.com>
2023-05-14 02:45:46 +0200

    add metadata information

--- src/data_collection_ncbi/scripts/get_assembly_accession_ids.sh
+++ src/data_collection_ncbi/scripts/get_assembly_accession_ids.sh
@@ -11,5 +11,5 @@ biosamples_accession_ids=$(awk -F , '{print $1}' "$input_filepath" | tail -n +2)
 echo "Getting assembly accessionIds for total of $(echo $biosamples_accession_ids | wc -w) biosamples..."
 queryIds=$(echo $biosamples_accession_ids | sed 's/ / OR /g')
 esearch -db biosample -query "$queryIds" | elink -target assembly | esummary | xtract \
--pattern DocumentSummary -element AssemblyAccession,SpeciesTaxid > "$output_filepath"
+-pattern DocumentSummary -element BioSampleAccn,AssemblyAccession,SpeciesTaxid > "$output_filepath"
 echo "Done. Output written to $output_filepath"
