5b7b196e9e7e5505bee8872fc2da673118530c9e
Geovanny Risco <geovannyalexanderr@gmail.com>
2023-10-15 13:43:32 +0000

    update config file with test samples

--- workflows/resfinder/run_on_azure.sh
+++ workflows/resfinder/run_on_azure.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 export AZ_BLOB_PREFIX=snakemake # This must be the container name
-export AZ_BATCH_ACCOUNT_URL=https://batchmlinargs01.westeurope.batch.azure.com # Replace with your Batch account URL
-export AZ_BATCH_ACCOUNT_KEY="XXXXX" # Get this information from the Azure Portal or Azure CLI
 export AZ_BLOB_ACCOUNT_URL="https://${stgacct}.blob.core.windows.net/$sastoken" # Get this information from the Azure Portal or Azure CLI
 
 snakemake --config use_azure=true --kubernetes \
